7729d16aa76bc18147081a1e6970fb3df5664e02
popper2710 <hige1332@yahoo.co.jp>
2020-06-04 02:58:23 +0900

    [add] add secret_dummy.py

---
+++ trend_analyze/config/secret_dummy.py
@@ -0,0 +1,21 @@
+# Twitter API KEY LIST
+CONSUMER_KEY = '***'
+CONSUMER_SECRET = '***'
+ACCESS_TOKEN = '***'
+ACCESS_TOKEN_SECRET = '***'
+
+
+# DB settings
+DB_HOST = "***"
+DB_PORT = "***"
+DB_NAME = "***"
+DB_PASSWORD = "***"
+DATABASE = "***"
+
+# Twitter email and password
+TWITTER_EMAIL = "***"
+TWITTER_PWD = r"***"
+
+# Twiiter User Information
+TEST_USER_ID = "***"
+TEST_USERNAME = '***'
