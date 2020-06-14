5cc302826bd6cc8a8a2c9c981126e30fafe45f82
popper2710 <hige1332@yahoo.co.jp>
2020-06-14 23:57:01 +0900

    [add] add base test for manage.py

---
+++ trend_analyze/test/test_manage.py
@@ -0,0 +1,44 @@
+import unittest
+
+from trend_analyze.config import *
+from trend_analyze.src.manage import Manage
+
+
+class TestManage(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestManage, self).__init__(*args, **kwargs)
+        self.manage = Manage()
+
+    def test_update_trend_availables(self):
+        pass
+
+    def test_store_user_tweet(self):
+        pass
+
+    def test_store_user_tweet_n(self):
+        pass
+
+    def test_store_tweet_including_trend(self):
+        pass
+
+    def test_store_tweet_including_word(self):
+        pass
+
+    def test_store_tweet_including_word_n(self):
+        pass
+
+    def test_store_users_relation(self):
+        pass
+
+    def test_store_users_relation_n(self):
+        pass
+
+    def test_upgrade_user(self):
+        pass
+
+    def test_get_limit_status(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
