f20874129e54f05149a0bb06ffa52ebb39e5c557
popper2710 <hige1332@yahoo.co.jp>
2020-06-22 23:58:29 +0900

    [add] add some test for manage.py
    - store_tweet_including_word
    - store_tweet_including_word_n
    - store_user_tweet
    - store_user_tweet_n

--- trend_analyze/test/test_manage.py
+++ trend_analyze/test/test_manage.py
@@ -1,9 +1,10 @@
 import unittest
+import datetime
 
 from trend_analyze.config import *
 from trend_analyze.src.manage import Manage
 from trend_analyze.src.db import session
-from trend_analyze.src.table_model import *
+from trend_analyze.src.fetch_data_from_api import ApiTwitterFetcher
 from trend_analyze.src.table_model import *
 
 
@@ -14,6 +15,7 @@ class TestManage(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestManage, self).__init__(*args, **kwargs)
         self.manage = Manage()
+        self.atf = ApiTwitterFetcher()
         self.session = session
 
     def setUp(self) -> None:
@@ -30,19 +32,37 @@ class TestManage(unittest.TestCase):
         self.assertLessEqual(time_diff.seconds, 600)
 
     def test_store_user_tweet(self):
-        pass
+        self.manage.store_user_tweet(TEST_USER_ID)
+        user_tweet = self.session.query(TableUser.updated_at)\
+            .filter(TableUser.t_user_id == TEST_USER_ID)
+        time_diff = datetime.now() - user_tweet[0]
+        self.assertLessEqual(time_diff.seconds, 600)
 
     def test_store_user_tweet_n(self):
-        pass
+        self.manage.store_user_tweet_n(TEST_USER_ID)
+        user_tweet = self.session.query(TableUser.updated_at) \
+            .filter(TableUser.t_user_id == TEST_USER_ID)
+        time_diff = datetime.now() - user_tweet[0]
+        self.assertLessEqual(time_diff.seconds, 600)
 
     def test_store_tweet_including_trend(self):
-        pass
+        self.manage.store_tweet_including_trend(rank=1)
+        top_trend_word = self.atf.fetch_current_trends(JAPAN_WOEID)[0]["trends"][0]["name"]
+        trend_tweet = self.session.query(TableTweet.text) \
+            .filter(TableTweet.user.t_user_id != TEST_USER_ID and
+                    TableTweet.created_at >= datetime.today() - datetime.timedelta(days=1))
+        self.assertIn(top_trend_word, trend_tweet[0])
 
     def test_store_tweet_including_word(self):
         pass
 
     def test_store_tweet_including_word_n(self):
-        pass
+        self.manage.store_tweet_including_trend(rank=2)
+        second_trend_word = self.atf.fetch_current_trends(JAPAN_WOEID)[0]["trends"][1]["name"]
+        trend_tweet = self.session.query(TableTweet.text) \
+            .filter(TableTweet.user.t_user_id != TEST_USER_ID and
+                    TableTweet.created_at >= datetime.today() - datetime.timedelta(days=1))
+        self.assertIn(second_trend_word, trend_tweet[0])
 
     def test_store_users_relation(self):
         pass
@@ -60,7 +80,6 @@ class TestManage(unittest.TestCase):
         limit = self.manage.get_limit_status()
         # It can arises Error by deviation.
         self.assertIn("rate_limit_context", limit)
-
 
 
 if __name__ == '__main__':
