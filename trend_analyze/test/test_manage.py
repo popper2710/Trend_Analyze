a6eeeeb379db82e777cb08b89f2d3549ffd479b1
popper2710 <hige1332@yahoo.co.jp>
2020-06-27 23:55:46 +0900

    [fix] fix test for manage.py

--- trend_analyze/test/test_manage.py
+++ trend_analyze/test/test_manage.py
@@ -12,6 +12,7 @@ class TestManage(unittest.TestCase):
     """
     test class for manage.py
     """
+
     def __init__(self, *args, **kwargs):
         super(TestManage, self).__init__(*args, **kwargs)
         self.manage = Manage()
@@ -32,58 +33,62 @@ class TestManage(unittest.TestCase):
         self.assertLessEqual(time_diff.seconds, 600)
 
     def test_store_user_tweet(self):
+        start = datetime.now()
         self.manage.store_user_tweet(TEST_USER_ID)
-        user_tweet = self.session.query(TableUser.updated_at)\
-            .filter(TableUser.t_user_id == TEST_USER_ID)
-        time_diff = datetime.now() - user_tweet[0]
-        self.assertLessEqual(time_diff.seconds, 600)
+        user_tweet = self.session.query(TableTweet) \
+            .filter(TableTweet.user.t_user_id == TEST_USER_ID and TableTweet.updated_at > start)
+        self.assertEqual(TEST_USER_ID, user_tweet.user.t_user_id)
 
     def test_store_user_tweet_n(self):
+        start = datetime.now()
         self.manage.store_user_tweet_n(TEST_USER_ID)
-        user_tweet = self.session.query(TableUser.updated_at) \
-            .filter(TableUser.t_user_id == TEST_USER_ID)
-        time_diff = datetime.now() - user_tweet[0]
-        self.assertLessEqual(time_diff.seconds, 600)
+        user_tweet = self.session.query(TableTweet) \
+            .filter(TableTweet.user.t_user_id == TEST_USER_ID and TableTweet.updated_at > start)
+        self.assertEqual(TEST_USER_ID, user_tweet.user.t_user_id)
 
     def test_store_tweet_including_trend(self):
+        start = datetime.now()
         self.manage.store_tweet_including_trend(rank=1)
         top_trend_word = self.atf.fetch_current_trends(JAPAN_WOEID)[0]["trends"][0]["name"]
         trend_tweet = self.session.query(TableTweet.text) \
             .filter(TableTweet.user.t_user_id != TEST_USER_ID and
-                    TableTweet.created_at >= datetime.today() - datetime.timedelta(days=1))
-        self.assertIn(top_trend_word, trend_tweet[0])
+                    TableTweet.updated_at > start)
+        self.assertIn(top_trend_word, trend_tweet[0].text.lower())
 
     def test_store_tweet_including_word(self):
+        start = datetime.now()
         testword = "Analyze"
         self.manage.store_tweet_including_word(testword)
-        including_tweet = self.session.query(TableTweet.text).order_by(TableTweet.updated_at).first()
+        including_tweet = self.session.query(TableTweet).filter(TableTweet.updated_at > start)
         self.assertIn(testword, including_tweet[0].lower())
 
     def test_store_tweet_including_word_n(self):
+        start = datetime.now()
         testword = "Essays"
         self.manage.store_tweet_including_word_n(testword)
-        including_tweet = self.session.query(TableTweet.text).order_by(TableTweet.updated_at).first()
+        including_tweet = self.session.query(TableTweet.text).filter(TableTweet.updated_at > start)
         self.assertIn(testword, including_tweet[0].lower())
 
     def test_store_users_relation(self):
         start = datetime.now()
         self.manage.store_users_relation(TEST_USER_ID)
-        user_relations = self.session.query(TableUsersRelation)\
+        user_relations = self.session.query(TableUsersRelation) \
             .filter(TableUsersRelation.user_id == TEST_USER_ID and TableUsersRelation.updated_at >= start)
-        self.assertGreaterEqual(1, user_relations)
+        self.assertGreaterEqual(1, len(user_relations))
 
     def test_store_users_relation_n(self):
         start = datetime.now()
         self.manage.store_users_relation_n(TEST_USER_ID)
-        user_relations = self.session.query(TableUsersRelation)\
+        user_relations = self.session.query(TableUsersRelation) \
             .filter(TableUsersRelation.user_id == TEST_USER_ID and TableUsersRelation.updated_at >= start)
-        self.assertGreaterEqual(1, user_relations)
+        self.assertGreaterEqual(1, len(user_relations))
 
     def test_upgrade_user(self):
+        start = datetime.now()
         self.manage.upgrade_user(TEST_USER_ID)
-        user = self.session.query(TableUser).filter(TableUser.t_user_id == TEST_USER_ID)
-        time_diff = datetime.now() - user.updated_at
-        self.assertLessEqual(time_diff.seconds, 600)
+        user = self.session.query(TableUser) \
+            .filter(TableUser.t_user_id == TEST_USER_ID and TableUser.updated_at >= start)
+        self.assertGreaterEqual(1, len(user))
 
     def test_get_limit_status(self):
         limit = self.manage.get_limit_status()
