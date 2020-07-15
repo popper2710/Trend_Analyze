b8bb49b5813751b02b3b0c9579e740be2ed2b0fc
popper2710 <hige1332@yahoo.co.jp>
2020-07-15 23:59:17 +0900

    [add] add test to check that it enables to prevent duplicate tweet

--- trend_analyze/test/test_controller.py
+++ trend_analyze/test/test_controller.py
@@ -5,6 +5,7 @@ from trend_analyze.config import *
 from trend_analyze.src.controller import Controller
 from trend_analyze.src.db import session
 from trend_analyze.src.table_model import *
+from trend_analyze.src.fetch_data_from_api import ApiTwitterFetcher
 
 
 class TestController(unittest.TestCase):
@@ -14,7 +15,8 @@ class TestController(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(TestController, self).__init__(*args, **kwargs)
-        self.controller = Controller
+        self.controller = Controller()
+        self.atf = ApiTwitterFetcher()
 
     def setUp(self) -> None:
         os.environ['TREND_ANALYZE_ENV'] = 'test'
@@ -25,6 +27,16 @@ class TestController(unittest.TestCase):
 
     def tearDown(self) -> None:
         os.environ['TREND_ANALYZE_ENV'] = TREND_ANALYZE_ENV
+
+    def test_prevent_duplicate(self):
+        start = datetime.now()
+        tweet = self.atf.fetch_user_tweet(TEST_USER_ID, count=1)
+        for i in range(5):
+            self.controller.insert_user(tweet)
+        user_tweet = session.query(TableTweet) \
+            .filter(TableTweet.user.t_user_id == TEST_USER_ID and TableTweet.updated_at > start).all()
+        self.assertEqual(1, len(user_tweet))
+
 
     def test_execute_sql(self) -> None:
         """
