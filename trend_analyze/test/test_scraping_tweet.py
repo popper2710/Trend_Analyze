4b41f9de36c4b38d1ec8f6cde80860c9ddf2567d
popper2710 <hige1332@yahoo.co.jp>
2020-08-01 23:57:34 +0900

    [fix] fix tests in test_scraping_tweet.py

--- trend_analyze/test/test_scraping_tweet.py
+++ trend_analyze/test/test_scraping_tweet.py
@@ -11,7 +11,6 @@ class TestScrapingTweet(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(TestScrapingTweet, self).__init__(*args, **kwargs)
-        self.ts = TwitterScraper()
 
     def setUp(self) -> None:
         os.environ['TREND_ANALYZE_ENV'] = 'test'
@@ -20,11 +19,17 @@ class TestScrapingTweet(unittest.TestCase):
         os.environ['TREND_ANALYZE_ENV'] = TREND_ANALYZE_ENV
 
     def test_follower_list(self):
-        follower_list = self.ts.follower_list(TEST_USERNAME)
-        self.assertNotEqual(follower_list, [])
+        with TwitterScraper() as ts:
+            follower_list = ts.follower_list(TEST_USERNAME)
+            self.assertIsInstance(follower_list, list)
+            self.assertNotEqual(follower_list, [])
 
     def test_following_list(self):
-        follower_list = self.ts.following_list(TEST_USERNAME)
-        self.assertNotEqual(follower_list, [])
+        with TwitterScraper() as ts:
+            following_list = ts.following_list(TEST_USERNAME)
+            self.assertIsInstance(following_list, list)
+            self.assertNotEqual(following_list, [])
+
     def test_name_to_id(self):
-        self.assertEqual(TEST_USER_ID, self.ts.name_to_id(TEST_USERNAME))
+        with TwitterScraper() as ts:
+            self.assertEqual(TEST_USER_ID, ts.name_to_id(TEST_USERNAME))
