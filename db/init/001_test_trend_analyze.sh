b3bc1445b6050f767e34f759d982b1ae0b59fcff
popper2710 <hige1332@yahoo.co.jp>
2020-08-05 23:41:48 +0900

    [fix] replace 001_test_trend_analyze.sh to init.sql

--- db/init/001_test_trend_analyze.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-echo "CREATE DATABASE IF NOT EXISTS \`test_trend_analyze\` ;" | "${mysql[@]}"
-echo "GRANT ALL ON \`test_trend_analyze\`.* TO '"$MYSQL_USER"'@'%' ;" | "${mysql[@]}"
-echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
-
-"${mysql[@]}" < /docker-entrypoint-initdb.d/test_trend_analyze.sql_
