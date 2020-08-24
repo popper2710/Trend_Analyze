ec24286454570e15e3bdb02c92d88c2f0127d4dc
popper2710 <hige1332@yahoo.co.jp>
2020-08-24 23:59:22 +0900

    [fix] fix return value in ApiTwitterFecher.fetch_user_tweet

--- trend_analyze/src/fetch_data_from_api.py
+++ trend_analyze/src/fetch_data_from_api.py
@@ -89,7 +89,7 @@ class ApiTwitterFetcher:
         :type count: int
         :param user_id:
         :type user_id: str
-        :return tweet_list:[Generator(list[Tweet])]
+        :return Generator(Tweet)
 
         [!!] If Target user is protected, it cannot receive tweet(response code is '401').
         """
@@ -97,13 +97,10 @@ class ApiTwitterFetcher:
         try:
             for page in tweepy.Cursor(self.api.user_timeline, user_id=user_id,
                                       tweet_mode="extended", count=count, *args, **kwargs).pages():
-                tweet_list = []
-                t_append = tweet_list.append
                 for tweet in page:
                     m_t = self.ctm.from_tpy_tweet(tweet)
                     m_t.is_official = True
-                    t_append(m_t)
-                yield tweet_list
+                    yield m_t
 
         except tweepy.error.TweepError as err:
             self._q_logging(err.reason)
