dc35c5530e0b71964181518d01bafa22d4a0f6ff
popper2710 <hige1332@yahoo.co.jp>
2020-10-08 23:35:36 +0900

    [fix] fix typo error and some bugs in scraping_tweet.py

--- trend_analyze/src/scraping_tweet.py
+++ trend_analyze/src/scraping_tweet.py
@@ -3,9 +3,16 @@ import logging.config
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import random
 
 from trend_analyze.src.model import User
 from trend_analyze.config import *
+
+user_agent_list = ['Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+                   'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+                   'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+                   'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+                   'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko', ]
 
 
 class TwitterScraper:
@@ -17,9 +24,8 @@ class TwitterScraper:
     def __init__(self):
         logging.config.dictConfig(LOGGING_DICT_CONFIG)
         self.logger = logging.getLogger('scraping_tweet')
-        self.m_twitter_url = "https://mobile.twiiter.com"
+        self.m_twitter_url = "https://mobile.twitter.com"
         self.twitter_url = "https://twitter.com"
-        self.user_agent = 'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)'
 
     # ========================================[public method]=========================================
 
@@ -30,7 +36,7 @@ class TwitterScraper:
         :return: User
         """
         url = f"{self.m_twitter_url}/{username}"
-        headers = {"User-Agent": self.user_agent}
+        headers = {"User-Agent": random.choice(user_agent_list)}
         res = requests.get(url, headers=headers)
         html = BeautifulSoup(res.text, "lxml")
 
@@ -39,9 +45,9 @@ class TwitterScraper:
         user.screen_name = html.select_one("span.screen-name").text
         user.location = html.select_one("div.location").text
         user.description = html.select_one("td > div.bio > div").get_text().strip()
-        user.statuses_count = html.select_one("td:nth-child(1) > div.statnum")
-        user.following_count = html.select_one("td:nth-child(2) > a > div.statnum")
-        user.follower_count = html.select_one("td.stat.stat-last > a > div.statnum")
+        user.statuses_count = int(html.select_one("td:nth-child(1) > div.statnum").text)
+        user.following_count = int(html.select_one("td:nth-child(2) > a > div.statnum").text)
+        user.follower_count = int(html.select_one("td.stat.stat-last > a > div.statnum").text)
         user.updated_at = datetime.now()
 
         return user
@@ -93,7 +99,7 @@ class TwitterScraper:
         :type url: str
         :return: List[str(username)]
         """
-        headers = {"User-Agent": self.user_agent}
+        headers = {"User-Agent": random.choice(user_agent_list)}
         user_list = list()
         user_push = user_list.append
         while True:
