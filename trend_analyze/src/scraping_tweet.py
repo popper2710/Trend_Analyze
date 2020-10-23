95db1b9ef20c6cf1cda20e10a6faeacbd31a9837
popper2710 <hige1332@yahoo.co.jp>
2020-10-23 23:55:42 +0900

    [fix] remove inappropriate agents

--- trend_analyze/src/scraping_tweet.py
+++ trend_analyze/src/scraping_tweet.py
@@ -11,9 +11,7 @@ from trend_analyze.config import *
 
 user_agent_list = ['Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
                    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
-                   'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
-                   'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
-                   'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko', ]
+                   'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)']
 
 
 class TwitterScraper:
